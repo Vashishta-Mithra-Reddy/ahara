91644ee64822893bc1e21a7b46a2e99503ef2de6
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-11-03 03:03:21 +0530

    fix

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,3 +1,23 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 
-export const authClient = createAuthClient({});
+
+// export const authClient = createAuthClient({
+//     plugins: [inferAdditionalFields({
+//       user: {
+//         onboardingCompleted: {
+//           type: 'boolean'
+//         },
+//         onboardingCompletedAt: {
+//           type: 'date'
+//         }
+//       }
+//     })
+//   ]
+// });
+
+import type { auth } from "@ahara/auth";
+
+export const authClient = createAuthClient({
+  plugins: [inferAdditionalFields<typeof auth>()],
+});
